7cb12efb543366c09e0388d8a8b7fb89e6e0881e
Satziy <pihlajamaa.satu@gmail.com>
2023-09-03 19:31:36 +0300

    loppu häämöttää

--- src/Lucky7.java
+++ src/Lucky7.java
@@ -7,88 +7,61 @@ class Lucky7 {
 
 	Random random = new Random();
 
-	
-	
-	int randomNumber = random.nextInt(10);
-	int randomNumber2 = random.nextInt(10);
-	int randomNumber3 = random.nextInt(10);
-//	int[] numbers = {randomNumber, randomNumber2, randomNumber3};
 	int luckyNumber = 7;
 	int alkuRaha = 5;
-	// int voittoSumma = 0;
 	int summaNyt =alkuRaha;
 	String vastaus;
 	
+	//Pahoittelut finglish -sekoituksesta :D
+	//Peliohjeet
 	System.out.println("Lucky7\nPeliohjeet: Sinulla on alussa 5€ rahaa. Joka peli maksaa 1€. "
-			+"Jos yksi arvotuista numeroista on seitsemän, voitat 3€.\nJos kaksi numeroa on seitsemän, voitat 5€. Jos kolme numeroa on oikein, voitat 7€.\nPeli loppuu kun rahat loppuvat.\n ");
-	
-	do {
+	+"Jos yksi arvotuista numeroista on seitsemän, voitat 3€.\nJos kaksi numeroa on seitsemän, voitat 5€. Jos kolme numeroa on oikein, voitat 7€.\nPeli loppuu kun rahat loppuvat.\n ");
 	
 	
+	//looppi alkaa
 	
-	
-		System.out.println("Numerosi ovat " + randomNumber +"," + randomNumber2 + (",") + randomNumber3);
-	
-		// for (summaNyt = alkuRaha; summaNyt <=0; summaNyt++)
+do {
+	int randomNumber = random.nextInt(10);
+	int randomNumber2 = random.nextInt(10);
+	int randomNumber3 = random.nextInt(10);
 		
-		
-			
-			
-		//while (summaNyt <=0)
+			//Ilmoittaa pelaajalle mitkä ovat random-numerot
+			System.out.println("Numerosi ovat " + randomNumber +"," + randomNumber2 + (",") + randomNumber3);
+	
 		
 	 	if (randomNumber == luckyNumber && randomNumber2 == luckyNumber && randomNumber3 ==luckyNumber)
-	 	
-	 		{System.out.println("Onnea! Voitit 7 €");
+	  		{System.out.println("Onnea! Voitit 7 €");
 	 		summaNyt = summaNyt +7; }
 	 
 	 	else if (randomNumber == luckyNumber && randomNumber2 == luckyNumber || randomNumber2 == luckyNumber && randomNumber3==luckyNumber || randomNumber ==luckyNumber && randomNumber3 == luckyNumber)
-		 
-	 		{System.out.println("Onnea! Voitit 5€."); 
+		 	{System.out.println("Onnea! Voitit 5€."); 
 	 		summaNyt = summaNyt + 5;}
 	 
 	 	else if (randomNumber == luckyNumber || randomNumber2 == luckyNumber || randomNumber3 == luckyNumber)
-	 
-	 		{System.out.println ("Onnea! Voitit 3€."); 
+	  		{System.out.println ("Onnea! Voitit 3€."); 
 	 		summaNyt = summaNyt +3; }
 	 	
 		else 
-	 
-	 		{System.out.println("Hävisit.");
+	  		{System.out.println("Hävisit.");
 	 		summaNyt = summaNyt -1;
 	 		}
 	 		
-	
-	 	   System.out.println("Pääomasi on " + summaNyt + "€.");
+	 		//Ilmoittaa pelaajalle mikä on hänen alkupääomansa nyt
+	 	   	System.out.println("Pääomasi on " + summaNyt + "€.");
 	 	   
-	 	   System.out.println("Haluatko jatkaa peliä? Vastaa k/e ja paina Enter.");
-	 	   vastaus = in.nextLine();	
-	 	  if (vastaus.equals("e"))
+	 	   	System.out.println("Haluatko jatkaa peliä? Vastaa k/e ja paina Enter.");
+	 	   	vastaus = in.nextLine();
+	 	   
+	 	 if (vastaus.equals("e") || summaNyt ==0)
 	 	     {System.out.println("Peli loppui.");
 	 	   break; }
-	 	  
-	 	  if (vastaus.equals("k"));
-	 	  
-	 	  {System.out.println("Peli loppui.");}
-	 	  
-	 	  
-	 	   
+	 	  	
 	}
-	 	  while (summaNyt >0);    
-	    
 	
-	
-	
-	
-	     
-	     
+ while (summaNyt >0);  
+	   //looppi päättyy tähän
 	      
-	
-//	 for (int peli1 =alkuRaha; peli1 <=0; peli1++ ) {
-//	 
-//		 System.out.println("jotain");
-
-	
-	 
+		 
 
 	
 	}
