1a1347fb0f96f106c47396fc2ae21e9430ff7f8b
Satziy <pihlajamaa.satu@gmail.com>
2023-09-03 18:56:11 +0300

    Hommat jatkuu..

--- src/Lucky7.java
+++ src/Lucky7.java
@@ -6,6 +6,8 @@ class Lucky7 {
 	Scanner in = new Scanner(System.in);	
 
 	Random random = new Random();
+
+	
 	
 	int randomNumber = random.nextInt(10);
 	int randomNumber2 = random.nextInt(10);
@@ -14,51 +16,80 @@ class Lucky7 {
 	int luckyNumber = 7;
 	int alkuRaha = 5;
 	// int voittoSumma = 0;
-	int summaNyt =alkuRaha -1;
+	int summaNyt =alkuRaha;
+	String vastaus;
 	
 	System.out.println("Lucky7\nPeliohjeet: Sinulla on alussa 5€ rahaa. Joka peli maksaa 1€. "
 			+"Jos yksi arvotuista numeroista on seitsemän, voitat 3€.\nJos kaksi numeroa on seitsemän, voitat 5€. Jos kolme numeroa on oikein, voitat 7€.\nPeli loppuu kun rahat loppuvat.\n ");
+	
+	do {
+	
+	
 	
 	
 		System.out.println("Numerosi ovat " + randomNumber +"," + randomNumber2 + (",") + randomNumber3);
 	
 		// for (summaNyt = alkuRaha; summaNyt <=0; summaNyt++)
 		
-	//do 
+		
+			
 			
 		//while (summaNyt <=0)
 		
 	 	if (randomNumber == luckyNumber && randomNumber2 == luckyNumber && randomNumber3 ==luckyNumber)
 	 	
 	 		{System.out.println("Onnea! Voitit 7 €");
-	 		summaNyt = alkuRaha +7; }
+	 		summaNyt = summaNyt +7; }
 	 
 	 	else if (randomNumber == luckyNumber && randomNumber2 == luckyNumber || randomNumber2 == luckyNumber && randomNumber3==luckyNumber || randomNumber ==luckyNumber && randomNumber3 == luckyNumber)
 		 
 	 		{System.out.println("Onnea! Voitit 5€."); 
-	 		summaNyt = alkuRaha + 5;}
+	 		summaNyt = summaNyt + 5;}
 	 
 	 	else if (randomNumber == luckyNumber || randomNumber2 == luckyNumber || randomNumber3 == luckyNumber)
 	 
 	 		{System.out.println ("Onnea! Voitit 3€."); 
-	 		summaNyt = alkuRaha +3; }
+	 		summaNyt = summaNyt +3; }
 	 	
 		else 
 	 
 	 		{System.out.println("Hävisit.");
-	 		summaNyt =alkuRaha -1; }
+	 		summaNyt = summaNyt -1;
+	 		}
 	 		
 	
 	 	   System.out.println("Pääomasi on " + summaNyt + "€.");
-	 
+	 	   
+	 	   System.out.println("Haluatko jatkaa peliä? Vastaa k/e ja paina Enter.");
+	 	   vastaus = in.nextLine();	
+	 	  if (vastaus.equals("e"))
+	 	     {System.out.println("Peli loppui.");
+	 	   break; }
+	 	  
+	 	  if (vastaus.equals("k"));
+	 	  
+	 	  {System.out.println("Peli loppui.");}
+	 	  
+	 	  
+	 	   
+	}
+	 	  while (summaNyt >0);    
+	    
+	
+	
+	
+	
+	     
+	     
+	      
+	
 //	 for (int peli1 =alkuRaha; peli1 <=0; peli1++ ) {
 //	 
 //		 System.out.println("jotain");
-	 
-	//while (alkuRaha <=0); {
-		 //System.out.println("Peli loppui.");
-	 
+
 	
+	 
+
 	
 	}
 	
