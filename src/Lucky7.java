066127e6a6d4fe0e660f44064678d55a7309bbcd
Satziy <pihlajamaa.satu@gmail.com>
2023-09-03 15:01:21 +0300

    lisäyksiä

--- src/Lucky7.java
+++ src/Lucky7.java
@@ -1,9 +1,31 @@
-
-public class Lucky7 {
+import java.util.Scanner;
+import java.util.Random;
+class Lucky7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner in = new Scanner(System.in);	
 
+	Random random = new Random();
+	
+	int randomNumber = random.nextInt(10);
+	int randomNumber2 = random.nextInt(10);
+	int randomNumber3 = random.nextInt(10);
+	int[] numbers = {randomNumber, randomNumber2, randomNumber3};
+	int luckyNumber = 7;
+	
+	System.out.println("Numerosi ovat " + randomNumber +"," + randomNumber2 + (",") + randomNumber3);
+	
+	 if (randomNumber == luckyNumber || randomNumber2 == luckyNumber || randomNumber3 ==luckyNumber)
+	 	
+	 {
+		 System.out.println("Onnea!");
+	 }
+	 else 
+		 
+	 {System.out.println("Hävisit");}
+	 
+	 
+	 
 	}
-
+    
 }
